bc8f77580e7a9c3cc28c1053ce6e69bf8088582e
Jose M <Jose.m.garcia1931@gmail.com>
2020-02-04 02:04:03 -0600

    Update wsgi.py

--- tutorial/tutorial/wsgi.py
+++ tutorial/tutorial/wsgi.py
@@ -7,11 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
